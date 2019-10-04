1ec2a39afb2b0b64c3521a2fa7cd3c4a4358b824
Gabriel Cruz <gabrielcruz023@users.noreply.github.com>
2019-10-04 02:39:58 -0300

    Melhoria no desempenho do código.
    A classe CubePile era desnecessária e foi substituída por uma função, can_be_piled(), que faz o mesmo trabalho, só que mais rápido. Deixei de tratar a quantidade de cubos porque não vi relevância, visto que a entrada é lida de stdin.

--- piling_up/piling_up.py
+++ piling_up/piling_up.py
@@ -2,43 +2,33 @@ from collections import deque
 from sys import stdin
 
 
-class CubePile:
-    def __init__(self, cubes, length=None):
-        maxlen = len(cubes) if not length else length
-        self.deque = deque(cubes, maxlen=maxlen)
+def can_be_piled(sides):
+    cubes = deque(sides)
+    last = None
 
-    def __iter__(self):
-        while self.deque:
-            if self.deque[0] >= self.deque[-1]:
-                yield self.deque.popleft()
-            else:
-                yield self.deque.pop()
+    while cubes:
+        if cubes[0] >= cubes[-1]:
+            cube = cubes.popleft()
+        else:
+            cube = cubes.pop()
 
-    def can_be_piled(self):
-        cubes = iter(self)
-        last = next(cubes)
+        if last is not None and last < cube:
+            return False
 
-        for cube in cubes:
-            if last < cube:
-                return False
+        last = cube
 
-            last = cube
-
-        return True
+    return True
 
 
 def piling_up():
     _, *tests = stdin.readlines()
 
-    for n_cubes, n_sides in zip(tests[0::2], tests[1::2]):
-        sides = (int(s) for s in n_sides.split())
-        cubes_qty = int(n_cubes)
-        pile = CubePile(sides, length=cubes_qty)
-
-        if pile.can_be_piled():
+    for sides in tests[1::2]:
+        if can_be_piled(int(cs) for cs in sides.split()):
             print('Yes')
-        else:
-            print('No')
+            continue
+
+        print('No')
 
 
 if __name__ == '__main__':
